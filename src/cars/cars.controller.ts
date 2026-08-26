e63af7af6e54ad4f29b024fdef8d91dd39a19598
KRocioCC <krociocalderon@gmail.com>
2026-08-26 13:37:08 -0400

    aplicando pipes y excepciones

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,8 +14,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCarById(@Param('id') id: string) {
-      console.log('id:', id);
-    return this.carsService.findOne(parseInt(id));
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOne(id);
   }
 }
